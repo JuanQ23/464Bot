125aabcad0248316479cbeec8395cf319ddd890a
JuanQ23 <89880885+JuanQ23@users.noreply.github.com>
2022-12-20 17:01:19 -0600

    Team
    Created a teams cog.

--- 464bot/cogs/privateroom.py
+++ 464bot/cogs/privateroom.py
@@ -1,9 +1,7 @@
-import asyncio
-
 import discord
 from discord import app_commands
 from discord.ext import commands
-from typing import List, Union
+from typing import List
 
 async def create_channels(interaction: discord.Interaction, member: discord.Member):
     
@@ -12,9 +10,7 @@ async def create_channels(interaction: discord.Interaction, member: discord.Memb
         return
     
     overwrites = {
-        interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-        interaction.guild.default_role: discord.PermissionOverwrite(create_instant_invite=False),
-        # users w/ default permission cannot read
+        interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False,create_instant_invite=False),
         interaction.guild.me: discord.PermissionOverwrite(read_messages=True),  # I can read (AKA server owner)
         member: discord.PermissionOverwrite(read_messages=True)  # the individual student can read.
     }
@@ -22,8 +18,8 @@ async def create_channels(interaction: discord.Interaction, member: discord.Memb
 
     category = await interaction.guild.create_category(name=member_name, overwrites=overwrites)
 
-    await category.create_text_channel(name=f"private")
-    await category.create_voice_channel(name=f"private voice")
+    await category.create_text_channel(name="private text")
+    await category.create_voice_channel(name="private voice")
     return category
 
 async def delete_groups(category: discord.CategoryChannel):
@@ -33,17 +29,6 @@ async def delete_groups(category: discord.CategoryChannel):
 
     await category.delete()
 
-
-class UserSelectView(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.value: list[discord.Member | discord.User]
-        
-    @discord.ui.select(cls=discord.ui.UserSelect, placeholder="search", min_values=1, max_values=3)
-    async def my_user_users(self, interaction: discord.Interaction, select: discord.ui.UserSelect):
-        self.value = select.values
-        await interaction.response.send_message("Creating, should be quick..", delete_after=5)
-        self.stop()
 
 class ChannelSelectView(discord.ui.View):
     def __init__(self):
@@ -65,8 +50,8 @@ class PrivateRooms(commands.Cog):
         self.bot = bot
         self.categories: list[discord.CategoryChannel] = []
 
-    @commands.is_owner()
     @app_commands.command()
+    @app_commands.default_permissions(manage_guild=True)
     async def create(self, interaction: discord.Interaction):
         """Creates private categories, private text channels, private voice channels for each student"""
 
@@ -88,8 +73,8 @@ class PrivateRooms(commands.Cog):
 
         await interaction.followup.send("done")
     
-    @commands.is_owner()     
     @app_commands.command()
+    @app_commands.default_permissions(manage_guild=True)
     async def destroy(self, interaction: discord.Interaction):
         """destroy selected categories, channels will not work"""
         
@@ -114,12 +99,12 @@ class PrivateRooms(commands.Cog):
         
         
         async with channel.typing():
-            for category in categories:
+            for category in categories: 
                 if isinstance(category, discord.CategoryChannel):
                     await delete_groups(category)
     
-    @commands.is_owner()
     @app_commands.command()
+    @app_commands.default_permissions(manage_guild=True)
     async def destroy_all_except(self, interaction: discord.Interaction):
         """Destroy all except that selected categories."""
         
@@ -137,10 +122,10 @@ class PrivateRooms(commands.Cog):
         await view.wait()
         
         #collecting response. creating list of categories to be saved.
-        to_delete = view.value
-        categories = [await element.fetch() for element in to_delete if element.type == discord.ChannelType.category]
-        channel: discord.TextChannel = interaction.channel
-
+        to_save = view.value
+        categories = [await element.fetch() for element in to_save if element.type == discord.ChannelType.category]
+        channel: discord.TextChannel = interaction.channel  
+        
         #deleting all other categories.
         async with channel.typing():
             for category in interaction.guild.categories:
@@ -149,72 +134,7 @@ class PrivateRooms(commands.Cog):
                 else:
                     await delete_groups(category)
     
-    @commands.is_owner()
-    @app_commands.command()
-    async def createteamcategory(self, interaction: discord.Interaction):
-        """creates a category for teams."""
     
-        #error handling
-        if interaction.guild is None:
-            await interaction.response.send_message("The guild appears to not be accessible by the bot. Possibly a permissions issue?")
-            return
-
-        cat = await interaction.guild.create_category(name="Teams")
-        await interaction.response.send_message("Team category created, please run the /team command to populate the category with teams.", delete_after=10)        
-        await cat.move(beginning = True)
-    
-    @commands.is_owner()
-    @app_commands.command()
-    async def team(self, interaction: discord.Interaction):
-        """creates team voice and text channels."""
-        
-        #error handling
-        if interaction.guild is None:
-            await interaction.response.send_message("the bot is not in any guilds")
-            return
-        if not isinstance(interaction.channel, discord.TextChannel):
-            print("The interaction channel is not a text channel. Unable to send message. Please initiate command in text channel.")
-            return
-        
-        category = discord.utils.get(interaction.guild.categories, name="Teams")
-        if category is None:
-            await interaction.response.send_message("unable to find the Teams Category. Please run the createTeams command.")
-            return
-        
-
-        view = UserSelectView()
-        await interaction.response.send_message(view=view)
-        await view.wait()
-
-
-        overwrites = {
-            interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False), # users w/ default permission cannot read
-            interaction.guild.me: discord.PermissionOverwrite(read_messages=True)  # I can read (AKA server owner)
-            }
-
-        teammates = view.value
-        text = await category.create_text_channel(name="Team 1", overwrites=overwrites)
-        vc = await category.create_voice_channel(name="Team 1", overwrites=overwrites)
-        
-        for member in teammates:
-            if not isinstance(member, discord.Member):
-                continue
-            
-            await text.set_permissions(target=member, read_messages=True, create_instant_invite=False)
-            await vc.set_permissions(target=member, read_messages=True, create_instant_invite= False)
-
-        
-
-    async def cog_app_command_error(self, interaction: discord.Interaction, error: discord.app_commands.AppCommandError):
-        print(repr(interaction.channel))
-        raise error
-
-
-    async def on_error(self, interaction, error, /):
-        print(repr(interaction.channel))
-        raise error
-
-
     @commands.Cog.listener()
     async def on_member_update(self, before:discord.Member, after: discord.Member):
         if before.nick != after.nick:
---
+++ 464bot/cogs/teams.py
@@ -0,0 +1,96 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from typing import List
+
+class UserSelectView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value: list[discord.Member | discord.User]
+        
+    @discord.ui.select(cls=discord.ui.UserSelect, placeholder="search", min_values=1, max_values=3)
+    async def my_user_users(self, interaction: discord.Interaction, select: discord.ui.UserSelect):
+        self.value = select.values
+        await interaction.response.send_message("Creating, should be quick..", delete_after=5)
+        self.stop()
+
+
+class Teams(commands.Cog):
+    """A set of commands that are used to automate creation and deletion of A team category and teams."""
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.teamnum = 1
+        
+    @app_commands.command()
+    @app_commands.default_permissions(manage_guild=True)
+    async def createteamcategory(self, interaction: discord.Interaction):
+        """creates a category for teams."""
+    
+        #error handling
+        if interaction.guild is None:
+            await interaction.response.send_message("The guild appears to not be accessible by the bot. Possibly a permissions issue?")
+            return
+
+        cat = await interaction.guild.create_category(name="Teams")
+        await interaction.response.send_message("Team category created, please run the /team command to populate the category with teams.", delete_after=10)        
+        await cat.move(beginning = True)
+    
+    @app_commands.command()
+    @app_commands.default_permissions(manage_guild=True)
+    async def team(self, interaction: discord.Interaction):
+        """creates team voice and text channels."""
+        #error handling / keeping the type hinter happy
+        if interaction.guild is None:
+            await interaction.response.send_message("the bot is not in any guilds")
+            return
+        if not isinstance(interaction.channel, discord.TextChannel):
+            print("The interaction channel is not a text channel. Unable to send message. Please initiate command in text channel.")
+            return
+        
+        category = discord.utils.get(interaction.guild.categories, name="Teams")
+        if category is None:
+            await interaction.response.send_message("unable to find the Teams Category. Please run the createTeams command.")
+            return
+        
+        #sending view, collecting response.
+        view = UserSelectView()
+        await interaction.response.send_message(view=view)
+        await view.wait()
+
+        
+        
+        #create team voice and text channel.
+        overwrites = {
+            interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False), # users w/ default permission cannot read
+            interaction.guild.me: discord.PermissionOverwrite(read_messages=True)  # I can read (AKA server owner)
+            }
+        teammates = view.value
+        
+        text = await category.create_text_channel(name=f"Team {self.teamnum}", overwrites=overwrites)
+        vc = await category.create_voice_channel(name=f"Team {self.teamnum}", overwrites=overwrites)
+        self.teamnum+=1 
+        for member in teammates:
+            if not isinstance(member, discord.Member):
+                continue
+            
+            await text.set_permissions(target=member, read_messages=True, create_instant_invite=False)
+            await vc.set_permissions(target=member,   read_messages=True, create_instant_invite=False)
+
+        
+
+    async def cog_app_command_error(self, interaction: discord.Interaction, error: discord.app_commands.AppCommandError):
+        print(repr(interaction.channel))
+        raise error
+
+
+    async def on_error(self, interaction, error, /):
+        print(repr(interaction.channel))
+        raise error
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Teams cog loaded")
+
+async def setup(bot):
+    await bot.add_cog(Teams(bot))
